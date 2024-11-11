45a3d40653a1f0f97c5e7528e6e4a8ae67b16c22
Maria Aparecidade Da silva de souza <wilkerjadson000@gmail.com>
2024-11-11 02:31:34 -0300

    Desafio hubbi concluido

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_URL_API: process.env.NEXT_PUBLIC_URL_API,
+  },
+
 };
 
 export default nextConfig;
